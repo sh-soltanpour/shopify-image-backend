77126cd828f630edc445fc6424f0fac242c64bca
Shahryar Soltanpour <shahryar.soltanpour@gmail.com>
2022-11-02 00:17:18 -0600

    Add dockerfile to the project

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const createError = require('http-errors'),
-    express = require('express'),
+const express = require('express'),
+    mongoose = require('mongoose'),
     path = require('path'),
-    cookieParser = require('cookie-parser'),
     logger = require('morgan'),
-    mongoose = require('mongoose');
+    cookieParser = require('cookie-parser');
 
-mongoose.connect("mongodb://localhost:27017/shopify");
-require("./models/User");
-require("./models/Image");
+const app = express();
+const PORT = 3000;
+mongoose.connect("mongodb://localhost:27017/shopify")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth/index');
-var imagesRouter = require('./routes/image/index');
+require('./models/user');
+require('./models/image');
 
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth/index');
+const imagesRouter = require('./routes/image/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,4 +47,65 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
+
+app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+)
+
+
+// const createError = require('http-errors'),
+//     express = require('express'),
+//     path = require('path'),
+//     cookieParser = require('cookie-parser'),
+//     logger = require('morgan'),
+//     mongoose = require('mongoose');
+//
+//
+//
+//
+//
+//
+// mongoose.connect("mongodb://localhost:27017/shopify");
+// require("./models/User");
+// require("./models/Image");
+//
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var authRouter = require('./routes/auth/index');
+// var imagesRouter = require('./routes/image/index');
+//
+//
+// var app = express();
+//
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+//
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/auth', authRouter);
+// app.use('/images', imagesRouter);
+//
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
+//
+// // error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
+//
+// module.exports = app;
